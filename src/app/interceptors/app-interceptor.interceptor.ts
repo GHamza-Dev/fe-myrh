911a716e59deb65a3a280df2fc550c39837b32a3
Ghamza-dev <hamza.gassai@gmail.com>
2023-01-03 16:09:43 +0100

    Create http interceptor

---
+++ src/app/interceptors/app-interceptor.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {LocalStorageService} from "../services/storage/local-storage.service";
+
+@Injectable()
+export class AppInterceptorInterceptor implements HttpInterceptor {
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const token = this.localStorageService.get('token');
+
+    if (token){
+
+      const clonedRequest = request.clone({
+        headers: request.headers.set("Authorization",
+          "Bearer " + token)
+      });
+
+      return next.handle(clonedRequest);
+    }
+
+    return next.handle(request);
+  }
+}
