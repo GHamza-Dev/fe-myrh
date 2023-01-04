353cbae483dba310707ad8e2552dec53b4301ac0
Ghamza-dev <hamza.gassai@gmail.com>
2023-01-04 22:30:33 +0100

    Fix interceptor

--- src/app/interceptors/app-interceptor.ts
+++ src/app/interceptors/app-interceptor.ts
@@ -7,23 +7,19 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {LocalStorageService} from "../services/storage/local-storage.service";
-import {Router} from "@angular/router";
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
 
   constructor(
-    private localStorageService: LocalStorageService,
-    private router: Router
+    private localStorageService: LocalStorageService
     ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     const token = this.localStorageService.get('token');
 
-    const route = this.router.routerState.snapshot.root;
-
-    if (token && route.data && route.data?.['guard'] === 'AuthorizeGuard'){
+    if (token){
 
       console.info("Intercepted by AppInterceptor")
 
