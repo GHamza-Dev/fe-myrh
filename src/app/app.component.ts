6381c955f3062009d254e67cab8827ab84048b06
Ghamza-dev <hamza.gassai@gmail.com>
2023-01-14 19:24:25 +0100

    Remove debugging code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,11 @@ import {JwtTokenService} from "./services/auth/jwt-token.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'fe-myrh';
 
   constructor(private authService: AuthService,
               private jwt: JwtTokenService) {
   }
 
-  ngOnInit(): void {
-    this.jwt.setToken(`eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWNydWl0ZXJAZ21haWwuY29tIiwicm9sZXMiOlsiUk9MRV9SRUNSVUlURVIiXSwiZXhwIjoxNjczNjgwMjk5LCJpYXQiOjE2NzM2NDQyOTl9.t1TZyhfS4ByaufewqMUewP3EXXuckUFnuwH4NzQVJc0`)
-    console.log(this.jwt.getDecodedToken())
-  }
 }
