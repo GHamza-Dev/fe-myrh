70883b1fa8dcb4a7d561129f92b28fe4b6b331b2
Ghamza-dev <hamza.gassai@gmail.com>
2023-01-14 19:03:28 +0100

    Navigation bar ui inhancement

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -10,6 +10,8 @@ import {AuthService} from "../../services/auth/auth.service";
 export class NavComponent implements OnInit{
   user!: Principal
 
+  dropDownMenuOpened = false
+
   constructor(private auth: AuthService) {
   }
 
